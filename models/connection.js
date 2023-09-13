5f1fb61c24378410dc560a2b9b1aa2ab0d0049ef
Dimitri <dfrancillette@gmail.com>
2023-09-13 17:29:41 +0200

    ajout d'une description dans le readme, ajout d'un exemple de data, ajout et utilisation du module dotenv

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://dimf:9mes9mjdegG8SdFI@cluster0.idjdkrw.mongodb.net/tickethack';
+const connectionString = process.env.CONNECT_DB;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
